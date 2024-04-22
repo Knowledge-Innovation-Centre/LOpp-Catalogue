d5c7113e4df181e8439c47c5d7eb0b2772052917
rokmlakar <roky.mlakar@gmail.com>
2024-04-22 19:09:05 +0200

    version update

--- wpemerge.php
+++ wpemerge.php
@@ -3,7 +3,7 @@
  * Plugin Name: Learning Opportunities Catalogue
  * Plugin URI: https://knowledgeinnovation.eu/
  * Description:
- * Version: 2.2.2
+ * Version: 2.3.2
  * Requires at least: 4.7
  * Requires PHP: 5.5.9
  * Author: Jure Jager & Carmen L. Padron - Knowledge innovation
